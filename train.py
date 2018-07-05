00de53377768c0715c69acdce67504c2954d5ac0
Raimundo <r.manterola1@gmail.com>
2018-07-05 02:34:57 -0400

    change glove dir

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ from keras.callbacks import History, ModelCheckpoint, EarlyStopping
 
 TRAIN_DIR = "data/train.json"
 TEST_DIR = "data/test.json"
-GLOVE_DIR = "glove/glove_300.txt"
+GLOVE_DIR = "glove/glove.6B.399d.txt"
 FILTER = ''' !"#$%&()*+,-./:;<=>?@[]^_{|}~\ '''
 WEIGHTS_FILEPATH = "model.hdf5"
 
