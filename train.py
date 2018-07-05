74723168c439df1f43b4d215372da55b21d5b926
Raimundo <r.manterola1@gmail.com>
2018-07-05 19:17:36 -0400

    remove test set

--- train.py
+++ train.py
@@ -20,10 +20,10 @@ train_data, test_data = preprocess.load_data(TRAIN_DIR, TEST_DIR)
 glove = preprocess.load_glove(GLOVE_DIR)
 
 Xc, Xq, Ys, Ye, mapper, maxContextLen, maxQuestionLen = preprocess.preprocess_data(train_data, glove, FILTER)
-Xct, Xqt, Yst, Yet, mappert, maxContextLent, maxQuestionLent = preprocess.preprocess_data(test_data, glove, FILTER)
+# Xct, Xqt, Yst, Yet, mappert, maxContextLent, maxQuestionLent = preprocess.preprocess_data(test_data, glove, FILTER)
 
 gen_train = textGen.generate_data(Xc, Xq, Ys, Ye, mapper, maxContextLen, maxQuestionLen, 128)
-gen_test = textGen.generate_data(Xct, Xqt, Yst, Yet, mappert, maxContextLen, maxQuestionLen, 128)
+# gen_test = textGen.generate_data(Xct, Xqt, Yst, Yet, mappert, maxContextLen, maxQuestionLen, 128)
 
 filepath="checkpoints/qa-weights-improvement-{epoch:02d}-{loss:.4f}.hdf5"
 checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
@@ -33,13 +33,13 @@ callbacks_list = [checkpoint, history]
 print("Max Question Length: {}".format(maxQuestionLen))
 print("Max Context Length: {}".format(maxContextLen))
 
-print("test data len {}".format(len(test_data)))
-print("gen_test {}".format(np.array(next(gen_test)[0]).shape))
+# print("test data len {}".format(len(test_data)))
+# print("gen_test {}".format(np.array(next(gen_test)[0]).shape))
 
 
 
 model = model.generate_model(maxContextLen, maxQuestionLen)
-model.fit_generator(gen_train, steps_per_epoch = Xq.shape[0]//128, epochs = 10, callbacks=callbacks_list, validation_data = gen_test, validation_steps = Xct.shape[0]//128)
+model.fit_generator(gen_train, steps_per_epoch = Xq.shape[0]//128, epochs = 10, callbacks=callbacks_list)
 model.save_weights(WEIGHTS_FILEPATH)
 with open('history.pickle', 'wb') as handle:
     pickle.dump(a, handle, protocol=pickle.HIGHEST_PROTOCOL)
